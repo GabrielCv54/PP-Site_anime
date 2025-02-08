2af979fda45d1b58a33dae931de8c108733810a2
GabrielCv54 <gabriel.cvieira@aluno.faculdadeimpacta.com.br>
2025-02-08 19:43:42 -0300

    Add files via upload

---
+++ PP-Site anime/Script/login.js
@@ -0,0 +1,7 @@
+document.getElementById('Login').addEventListener('click',)
+
+function login(){
+     var user_name = document.getElementsByClassName('login').innerHTML;
+     var password = document.getElementsByClassName('login').innerHTML; 
+
+}
