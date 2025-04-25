4d9fb64ef38826f6ceaaf2a6317534865018e362
Gabriel <gabriel.carvalhovieira123@gmail.com>
2025-04-24 21:37:06 -0300

    modificações

--- script/cadastro.js
+++ script/cadastro.js
@@ -4,11 +4,15 @@ function validateCadastro(){
    var email = document.getElementById('email').value;
    var nasc = document.getElementById('nasc').value;
    var nation = document.getElementById('nation').value;
+   const date = new Date();
+   date.getFullYear()
+   var val_nasc = date - nasc
 
-   if(nasc<18){
+   if(val_nasc < 18){
       alert('Só é possível realizar o cadastro com mais de 18 anos!!')
    }else{
-      alert('Continuação na próxima página de cadastro')
+      alert('Continuação na próxima página de cadastro!!')
+   
    }
 }
 
--- script/login.js
+++ script/login.js
@@ -1,27 +1,32 @@
+const user_name = document.getElementById('login').value;
+const password = document.getElementById('password').value;
+var submit = document.getElementById('submit').value;
+var recovery = document.getElementById('recovery').value;
+const icon = document.querySelector('#eye')
+
 function validate(){
-
-     const user_name = document.getElementById('login').value;
-     const password = document.getElementById('password').value;
-     var submit = document.getElementById('submit').value;
-     var recovery = document.getElementById('recovery').value;
-
-     
-
-
      //var invite = document.getElementById('')
      
      if(!user_name || !password){
       alert('O campo deve ser preenchido')
      }else if((user_name)){
           alert("Login feito com Sucesso")
-          location.href = '/view/home.html'    
+          location.href = 'home.html'    
      }else{
           alert('O email ou senha não são válidos')
      }
-
  
 }
 
+function showPassword(){
+     if(password.type == 'password'){
+          password.setAttribute('type','text');
+          icon.classList.add('hide');
+     }else{
+          password.setAttribute('type','password');
+          icon.classList.remove('hide');
+     }
+}
 
 
  
